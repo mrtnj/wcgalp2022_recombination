5ab9b9c2e5ebe1fdd193afd0056c2ce60bc2fc2a
Martin Johnsson <sorill@gmail.com>
2021-10-04 17:15:51 +0200

    Make script for running on server

--- R/simulate_chromosome_length.R
+++ R/simulate_chromosome_length.R
@@ -1,30 +1,44 @@
 ## Simulate chromosome length variation
 
 library(AlphaSimR)
+library(tibble)
 
 source("R/simulation_functions.R")
 
-  
-founders_short <- runMacs2(nInd = 1000,
-                           nChr = 10)
 
-founders_long <- runMacs2(nInd = 1000,
-                          nChr = 10,
-                          genLen = 2)
+args <- commandArgs(trailingOnly = TRUE)
 
-simulation_short <- make_simulation(founders_short)
-simulation_long <- make_simulation(founders_long)
 
-pop_short <- simulation_short$pop
-simparam_short <- simulation_short$simparam
+gen_length <- as.numeric(args[1])
+n_qtl_per_chr <- as.numeric(args[2])
+mean_dd <- as.numeric(args[3])
+var_dd <- as.numeric(args[4])
+out_file_population <- args[5]
+out_file_results <- args[6]
 
-pop_long <- simulation_long$pop
-simparam_long <- simulation_long$simparam
 
-result_short <- run_breeding(pop_short,
-                             10,
-                             simparam_short)
+founders <- runMacs2(nInd = 1000,
+                     nChr = 10,
+                     genLen = gen_length)
 
-result_long <- run_breeding(pop_long,
-                            10,
-                            simparam_long)
+simulation <- make_simulation(founders,
+                              n_qtl_per_chr = n_qtl_per_chr,
+                              mean_dd = mean_dd,
+                              var_dd = var_dd)
+
+pop <- simulation$pop
+simparam <- simulation$simparam
+
+generations <- run_breeding(pop,
+                            20,
+                            simparam)
+
+results <- get_stats(generations)
+
+
+saveRDS(generations,
+        file = out_file_population)
+
+saveRDS(results,
+        file = out_file_results)
+
