64a0923890656aaaae8aecca7d7aba669f348312
Martin Johnsson <sorill@gmail.com>
2021-11-15 17:03:43 +0100

    Create founders from a genome table from karyotype-based simulations

---
+++ R/simulate_karyotype_founders.R
@@ -0,0 +1,33 @@
+
+## Create founders for a simulation using real karyotypes
+
+
+library(AlphaSimR)
+library(readr)
+library(tibble)
+library(purrr)
+
+source("R/simulation_functions.R")
+
+
+args <- commandArgs(trailingOnly = TRUE)
+
+genome_table_file <- args[1]
+out_file <- args[2]
+
+genome_table <- read_tsv(genome_table_file)
+
+
+founder_chr <- pmap(list(bp = genome_table$length,
+                         genLen = genome_table$genetic_length,
+                         Ne = 1000,
+                         nInd = 1000,
+                         nChr = 1),
+                    runMacs2)
+
+founders <- Reduce(cChr, founder_chr)
+
+
+saveRDS(founders,
+        file = out_file)
+
