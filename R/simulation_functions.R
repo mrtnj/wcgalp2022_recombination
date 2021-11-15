342a1e2384367990d4ada9f5a111e1c6168d0b8c
Martin Johnsson <sorill@gmail.com>
2021-11-15 18:53:19 +0100

    Add functions for adjusting a simulated genetic map based on real genetic map

--- R/simulation_functions.R
+++ R/simulation_functions.R
@@ -208,3 +208,97 @@ compare_predicted_frequency_change <- function(pop1,
          delta_f = delta_f,
          predicted_delta_f = pred_delta_q)
 }
+
+
+
+##################################################################
+
+## Functions for using real genetic maps to adjust simulated maps
+
+
+
+## Linear interpolation of genetic marker position
+
+interpolate_cM <- function(marker_pos,
+                           bp_before,
+                           bp_after,
+                           cM_before,
+                           cM_after) {
+  
+  delta_bp <- bp_after - bp_before
+  delta_cM <- cM_after - cM_before
+  
+  cM_per_bp <- delta_cM/delta_bp
+  
+  cM_before +  cM_per_bp * (marker_pos - bp_before)
+}
+
+
+## Adjust a simulated genetic map based on real genetic map
+
+make_adjusted_map <- function(sim_map,
+                              real_map) {
+  
+  new_map <- vector(length = length(sim_map),
+                    mode = "list")
+  
+  real_map_chr <- split(real_map, real_map$chr)
+  
+  n_chr <- length(new_map)
+  
+  map_genetic_lengths <- map_dbl(real_map_chr,
+                                 function(chr) max(chr$position_cM))
+  
+  for (chr_ix in 1:n_chr) {
+    new_map[[chr_ix]] <- matrix(get_cM_positions_chr(sim_map[[chr_ix]],
+                                                     real_map_chr[[chr_ix]]),
+                                ncol = 1) /
+      map_genetic_lengths[chr_ix]
+  }
+  
+  new_map
+}
+
+
+## Get positions in cM based on a real map for simulated map, which is taken
+## to show the relative position in basepairs
+
+get_cM_positions_chr <- function(sim_map_chr,
+                                 real_map_chr) {
+  
+  chr_length <- max(real_map_chr$position_bp)
+  n_markers <- nrow(sim_map_chr)
+  
+  cM <- numeric(n_markers)
+  
+  for (marker_ix in 1:n_markers) {
+    
+    marker_pos <- chr_length * sim_map_chr[marker_ix]
+    
+    ix_before <- max(which(real_map_chr$position_bp < marker_pos))
+    ix_after <- min(which(real_map_chr$position_bp > marker_pos))
+    
+    ## First marker on chromosome
+    if (ix_after == 1) {
+      cM_position <- interpolate_cM(marker_pos,
+                                    bp_before = 0,
+                                    bp_after = real_map_chr$position_bp[ix_after],
+                                    cM_before = 0,
+                                    cM_after = real_map_chr$position_cM[ix_after])
+    } else {
+      ## Most cases
+      cM_position <- interpolate_cM(marker_pos,
+                                    bp_before = real_map_chr$position_bp[ix_before],
+                                    bp_after = real_map_chr$position_bp[ix_after],
+                                    cM_before = real_map_chr$position_cM[ix_before],
+                                    cM_after = real_map_chr$position_cM[ix_after])
+    }
+    
+    cM[marker_ix] <- cM_position
+    
+  }
+  
+  cM
+}
+
+
