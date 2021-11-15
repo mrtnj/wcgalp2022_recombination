1146dc35635a50804fcdfd643af3a65c0a53ff22
Martin Johnsson <sorill@gmail.com>
2021-11-15 17:07:10 +0100

    Save simparam

--- R/simulate_chromosome_length.R
+++ R/simulate_chromosome_length.R
@@ -2,6 +2,7 @@
 
 library(AlphaSimR)
 library(tibble)
+library(purrr)
 
 source("R/simulation_functions.R")
 
@@ -15,6 +16,7 @@ mean_dd <- as.numeric(args[3])
 var_dd <- as.numeric(args[4])
 out_file_population <- args[5]
 out_file_results <- args[6]
+out_file_simparam <- args[7]
 
 
 founders <- runMacs2(nInd = 1000,
@@ -36,9 +38,14 @@ generations <- run_breeding(pop,
 results <- get_stats(generations)
 
 
+
+
 saveRDS(generations,
         file = out_file_population)
 
 saveRDS(results,
         file = out_file_results)
+
+save(simparam,
+     file = out_file_simparam)
 
