8ef955df88777b27e59952e06162010bc0d234d0
Martin Johnsson <sorill@gmail.com>
2021-11-16 13:08:32 +0100

    Run both chicken and cattle; run dominance only

--- scripts/run_simulation_karyotype.sh
+++ scripts/run_simulation_karyotype.sh
@@ -1,40 +1,12 @@
 #!/bin/bash
 
-## Run simulations with approximation of chicken karyotype
+## Run simulations with approximation of real karyotypes
 
 set -eu
 
-if [ ! -d simulations/chicken_genome ]; then
-  mkdir -p simulations/chicken_genome
-fi
 
 
-## Additive
-
-
-if [ ! -d simulations/chicken_genome/additive/ ]; then
-  mkdir simulations/chicken_genome/additive/
-fi
-
-for TOT_QTL in 100 1000 10000; do
-
-  for REP in {1..20}; do
-    
-    if [ ! -d simulations/chicken_genome/additive/totqtl${TOT_QTL} ]; then
-      mkdir simulations/chicken_genome/additive/totqtl${TOT_QTL}
-    fi
-
-    Rscript R/simulate_chicken_genome.R \
-      ${TOT_QTL} \
-      0 \
-      0 \
-      simulations/chicken_genome/additive/totqtl${TOT_QTL}/populations_${REP}.Rds \
-      simulations/chicken_genome/additive/totqtl${TOT_QTL}/results_${REP}.Rds
-    
-  done
-  
-done
-
+## Chicken genome
 
 
 ## Dominance
@@ -51,12 +23,50 @@ for TOT_QTL in 100 1000 10000; do
       mkdir simulations/chicken_genome/dominance/totqtl${TOT_QTL}
     fi
 
-    Rscript R/simulate_chicken_genome.R \
+    Rscript R/simulate_karyotype.R \
       ${TOT_QTL} \
       0.2 \
       0.1 \
-      simulations/chicken_genome/dominance/totqtl${TOT_QTL}/populations_${REP}.Rds \
-      simulations/chicken_genome/dominance/totqtl${TOT_QTL}/results_${REP}.Rds
+      annotation/chicken_genome_table.txt \
+      simulations/chicken_genome/chicken_genome_founders.Rds \
+      simulations/chicken_genome/additive/totqtl${TOT_QTL}/populations_${REP}.Rds \
+      simulations/chicken_genome/additive/totqtl${TOT_QTL}/results_${REP}.Rds \
+      simulations/chicken_genome/additive/totqtl${TOT_QTL}/simparam_${REP}.Rds 
+    
+  done
+  
+done
+
+
+
+
+############################################################################
+
+## Cattle genome
+
+## Dominance
+
+if [ ! -d simulations/cattle_genome/dominance/ ]; then
+  mkdir simulations/cattle_genome/dominance/
+fi
+
+for TOT_QTL in 100 1000 10000; do
+
+  for REP in {1..20}; do
+    
+    if [ ! -d simulations/cattle_genome/dominance/totqtl${TOT_QTL} ]; then
+      mkdir simulations/cattle_genome/dominance/totqtl${TOT_QTL}
+    fi
+
+    Rscript R/simulate_karyotype.R \
+      ${TOT_QTL} \
+      0.2 \
+      0.1 \
+      annotation/cattle_genome_table.txt \
+      simulations/cattle_genome/cattle_genome_founders.Rds \
+      simulations/cattle_genome/dominance/totqtl${TOT_QTL}/populations_${REP}.Rds \
+      simulations/cattle_genome/dominance/totqtl${TOT_QTL}/results_${REP}.Rds \
+      simulations/cattle_genome/dominance/totqtl${TOT_QTL}/simparam_${REP}.Rds 
     
   done
   
