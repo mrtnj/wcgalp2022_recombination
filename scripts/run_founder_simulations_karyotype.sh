627e75c5712e4c37730092a1b23108efd98ff11a
Martin Johnsson <sorill@gmail.com>
2021-11-16 12:55:38 +0100

    Run cattle and chicke genome

--- scripts/run_founder_simulations_karyotype.sh
+++ scripts/run_founder_simulations_karyotype.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Run simulation to generate founder genomes for the chicken
+## Run simulation to generate founder genomes for cattle and chicken
 
 set -eu
 
@@ -9,5 +9,17 @@ if [ ! -d simulations/chicken_genome ]; then
 fi
 
 
-Rscript R/simulate_chicken_genome_founders.R
+Rscript R/simulate_karyotype_founders.R \
+  annotation/chicken_genome_table.txt \
+  simulations/chicken_genome/chicken_genome_founders.Rds
 
+
+if [ ! -d simulations/cattle_genome ]; then
+  mkdir -p simulations/cattle_genome
+fi
+
+
+Rscript R/simulate_karyotype_founders.R \
+  annotation/cattle_genome_table.txt \
+  simulations/cattle_genome/cattle_genome_founders.Rds
+  
