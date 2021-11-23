b04e22272d073085b0f119d80f2050f08a0232bb
Martin Johnsson <sorill@gmail.com>
2021-11-23 18:20:15 +0100

    Take away additive case

--- scripts/run_simulation_chr_length.sh
+++ scripts/run_simulation_chr_length.sh
@@ -9,42 +9,6 @@ if [ ! -d simulations/chromosome_length ]; then
 fi
 
 
-## Additive
-
-
-if [ ! -d simulations/chromosome_length/additive/ ]; then
-  mkdir simulations/chromosome_length/additive/
-fi
-
-for NQTL in 10 100 1000; do
-
-  for GEN_LENGTH in 0.5 1 2; do
-
-    for REP in {1..20}; do
-    
-      if [ ! -d simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL} ]; then
-        mkdir simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}
-      fi
-
-      Rscript R/simulate_chromosome_length.R \
-        ${GEN_LENGTH} \
-        ${NQTL} \
-        0 \
-        0 \
-        simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}/genotypes_${REP}.Rds \
-        simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}/phenotypes_${REP}.Rds \
-        simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}/results_${REP}.Rds \
-        simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}/simparam_${REP}.Rds \
-        simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}/genotypes_gs_${REP}.Rds \
-        simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}/phenotypes_gs_${REP}.Rds \
-        simulations/chromosome_length/additive/chrlen${GEN_LENGTH}_nqtl${NQTL}/results_gs_${REP}.Rds
-    
-    done
-  
-  done
-  
-done
-
 
 
 ## Dominance
