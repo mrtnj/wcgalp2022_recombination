653a97d4e767ee4c6fb8d2d3e2a9294dcb97dd81
Martin Johnsson <sorill@gmail.com>
2021-12-02 12:15:31 +0100

    Fix out paths; run both starting at 1 and at 10

--- scripts/run_simulation_linkage_equilibrium.sh
+++ scripts/run_simulation_linkage_equilibrium.sh
@@ -15,20 +15,24 @@ if [ ! -d simulations/linkage_equilibrium/dominance/ ]; then
   mkdir simulations/linkage_equilibrium/dominance/
 fi
 
-for TOT_QTL in 100 1000 10000; do
+for START in 1 10; do
 
-  for REP in {1..20}; do
+  for TOT_QTL in 100 1000 10000; do
+
+    for REP in {1..20}; do
     
-    if [ ! -d simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL} ]; then
-      mkdir simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}
-    fi
+      if [ ! -d simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}_start${START} ]; then
+        mkdir simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}_start${START}
+      fi
 
-    Rscript R/simulate_linkage_equilibrium.R \
-      simulations/cattle_genome/karyotype/dominance/totqtl${TOT_QTL}/simparam_${REP}.Rds \
-      simulations/cattle_genome/karyotype/dominance/totqtl${TOT_QTL}/genotypes_${REP}.Rds \
-      1 \
-      directional \
-      simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}/results.Rds
+      Rscript R/simulate_linkage_equilibrium.R \
+        simulations/cattle_genome/karyotype/dominance/totqtl${TOT_QTL}/simparam_${REP}.Rds \
+        simulations/cattle_genome/karyotype/dominance/totqtl${TOT_QTL}/genotypes_${REP}.Rds \
+        ${START} \
+        directional \
+        simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}_start${START}/results_${REP}.Rds
+        
+      done
     
   done
   
