4425d0ed53e91ae3f705e33f32f7806ad7cc2513
Martin Johnsson <sorill@gmail.com>
2021-12-02 22:17:14 +0100

    Paths

--- scripts/run_simulation_linkage_equilibrium.sh
+++ scripts/run_simulation_linkage_equilibrium.sh
@@ -30,7 +30,8 @@ for START in 1 10; do
         simulations/cattle_genome/karyotype/dominance/totqtl${TOT_QTL}/genotypes_${REP}.Rds \
         ${START} \
         directional \
-        simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}_start${START}/results_${REP}.Rds
+        simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}_start${START}/results_${REP}.Rds \
+        simulations/linkage_equilibrium/dominance/totqtl${TOT_QTL}_start${START}/generations_${REP}.Rds
         
       done
     
